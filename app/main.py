2ece256576ad79061b716b108b07b19265ea9a61
Kevin Yap <me@kevinyap.ca>
2017-10-20 19:25:36 -0700

    Add stub for audio upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -7,6 +7,27 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, world!"
+
+
+@app.route("/upload", methods=['POST'])
+def upload_audio():
+    """
+    Accepts a file as part of a POST request.
+    """
+
+    if 'file' not in request.files:
+        return "No file given in request.", 400
+
+    # TODO: Process the received audio data!
+    f = request.files['file']
+    data = f.read()
+
+    return jsonify({
+        'mimetype': f.mimetype,
+        'filename': f.filename,
+        'leading_bytes': str(data)[:64],
+        'size': len(data),
+    })
 
 
 if __name__ == "__main__":
