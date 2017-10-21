8f4550f167bdc7104d45229db474d42654962ae3
CoffeeTableEspresso <thiabaud.engelbrecht@gmail.com>
2017-10-21 15:34:44 -0700

    Update extract_observations.py
    finished ```extract_observations.py```.

--- collector/extract_observations.py
+++ collector/extract_observations.py
@@ -4,18 +4,20 @@ import numpy as np
 def extract_observations(signal, num_samples, samples_per_observation,
                          sample_rate=44100):
     """Converts signal into a table of observations for training data.
-
     Args:
         signal (np.ndarray): A 1xN array of raw audio data.
         samples_per_observation (int): A hyperparamter; the
             number of audio samples to place into each observation
             (row).
         sample_rate (int): The audio sample rate.
-
     Returns:
         np.ndarray: An array with num_samples / samples_per_observation
             rows and enough columns to evenly distrbute the audio data
             throughout the table.
     """
-    # Stub.
-    return np.zeros((0, 0))
+    channels = signal.shape[1]
+    num_samples = signal.shape[0]
+    mod = num_samples % samples_per_observation
+    truncated_signal = signal[:-mod]
+    observations = truncated_signal.reshape( (num_samples // samples_per_observation, -1) )
+    return observations
