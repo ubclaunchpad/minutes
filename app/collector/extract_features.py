f65f39d807f619930d9afc2610f436d5784dfcd9
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2017-11-11 14:43:53 -0800

    added mfcc features

--- app/collector/extract_features.py
+++ app/collector/extract_features.py
@@ -1,11 +1,18 @@
-import python_speech_features as psf
+from python_speech_features import mfcc
+from python_speech_features import logfbank
 import numpy as np
 
-def extract_features(observations):
-    """Input: array of observations (see output of `extract_observations.py`).
-       Output: array of features. 
-       Each row of observations is converted to features using `python_speech_features`.
+def extract_features(observations, sample_rate):
+    """observations: array of observations (see output of `extract_observations.py`).
+       sample_rate: sample rate
+       Each row of output is converted to features from `observations` using `python_speech_features`.
     """   
     # make features array 
+    # TODO: fix placeholder for width
+    width = 26 # number of columns of output array, calculated based on what features python_speech_features gives
+    features = np.zeros(observations.shape[0], width) 
     # convert rows to features 
+    for i in range(observations.shape[0]):
+        features[i, :] = mfcc(observations[i, :], sample_rate)
     # return features array
+    return features
