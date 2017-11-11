ead9d2f090226f439bc42e378b57c39979d53cba
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2017-11-11 12:43:58 -0800

    added skeleton for extract_features.py

---
+++ app/collector/extract_features.py
@@ -0,0 +1,10 @@
+import python_speech_features as psf
+import numpy as np
+
+def extract_features(observations):
+    """Input: array of observations (see output of `extract_observations.py`).
+       Output: array of features. 
+       Each row of observations is converted to features using `python_speech_features`.
+    """   
+    # convert rows to features in place
+    return observations
