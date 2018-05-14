b742d115f712705009b278710b78470cccae73b5
chadlagore <chad.d.lagore@gmail.com>
2018-05-13 22:43:40 -0700

    Speaker equality test

--- test/test_speaker.py
+++ test/test_speaker.py
@@ -1,20 +1,10 @@
-import os
-
-from minutes import Speaker
-import test.config as config
-
-SPEAKER1_AUDIO = os.path.join(config.FIXTURE_DIR, 'sample1.wav')
-SPEAKER2_AUDIO = os.path.join(config.FIXTURE_DIR, 'sample2.wav')
+import test.config as c
 
 
 def test_speaker_add_samples():
-    speaker1 = Speaker('speaker1')
-    speaker1.add_audio(SPEAKER1_AUDIO)
-    assert len(speaker1.audio) == 1
-
-    speaker1.add_audio(SPEAKER2_AUDIO)
-    assert len(speaker1.audio) == 2
+    assert len(c.SPEAKER1.audio) == 1
 
 
-def test_speaker_observations():
-    pass
+def test_speaker_equality():
+    assert c.SPEAKER1 == c.SPEAKER1
+    assert c.SPEAKER2 != c.SPEAKER1
